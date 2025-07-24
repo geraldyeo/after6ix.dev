507073ae1aa1f34904266f22ea37c26eb072166a
Gerald Yeo <gerald.yeo@gmail.com>
2025-07-24 16:46:48 +0800

    feat: add sst

---
+++ sst.config.ts
@@ -0,0 +1,18 @@
+export default $config({
+  app(input) {
+    return {
+      name: "after6ix",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    // const storage = await import("./infra/storage");
+    // await import("./infra/api");
+
+    return {
+    //   MyBucket: storage.bucket.name,
+    };
+  },
+});
