a80c1b92355b195e326d9ccbe98fe1168eb6dd5a
Gerald Yeo <gerald.yeo@gmail.com>
2025-07-28 16:12:31 +0800

    feat: add placeholders

---
+++ apps/site/src/App.tsx
@@ -0,0 +1,12 @@
+import { greeting } from '@after6ix/core';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>{greeting('After6ix Site')}</h1>
+      <p>Welcome to the After6ix website!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/core/src/utils.ts
@@ -0,0 +1,9 @@
+// Utility functions for @after6ix/core
+
+export function formatDate(date: Date): string {
+  return date.toISOString().split('T')[0];
+}
+
+export function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
