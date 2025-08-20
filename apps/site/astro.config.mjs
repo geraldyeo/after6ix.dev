8071c65642cab5c2eb3930f17666831be25deaf2
Gerald Yeo <gerald.yeo@gmail.com>
2025-08-20 15:13:37 +0800

    feat: add astro

---
+++ apps/site/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  // Enable TypeScript support
+  typescript: {
+    setup: true,
+  },
+});
